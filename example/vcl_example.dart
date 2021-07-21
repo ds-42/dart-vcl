26b1c864938adf538f0762de190c1eb5d974a5ff
Aleks <lai@tut.by>
2021-07-22 02:40:37 +0300

    empty project

---
+++ example/vcl_example.dart
@@ -0,0 +1,7 @@
+import 'package:vcl/vcl.dart';
+
+void main()
+{
+//  var awesome = Awesome();
+//  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/vcl.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library vcl;
+
+/// export 'src/vcl_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
