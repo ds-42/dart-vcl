15f489db4b6063ae7832f247cce76b24340f5327
Aleks <lai@tut.by>
2022-03-11 18:32:19 +0300

    add tryParse

--- lib/src/ext/metric.dart
+++ lib/src/ext/metric.dart
@@ -9,6 +9,28 @@ class TMetric
 
   TMetric(this.Value, [this.Unit = TMetricUnit.Pixel] );
 
+  static TMetric? tryParse(String data)
+  {
+    TMetric? parse(String val, TMetricUnit unit)
+    {
+      int? v = int.tryParse(val);
+      if(v==null)
+        return null;
+      return TMetric(v, unit);
+    }
+
+    var list;
+    list = data.split('px');
+    if(list.length==2 && list[1].isEmpty)
+      return parse(list[0], TMetricUnit.Pixel);
+
+    list = data.split('%');
+    if(list.length==2 && list[1].isEmpty)
+      return parse(list[0], TMetricUnit.Percent);
+
+    return parse(data, TMetricUnit.Pixel);
+  }
+
   String toString()
   {
     String unit = '';
