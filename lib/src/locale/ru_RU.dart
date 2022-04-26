90905115635c34dd96616f9f436f18dbeb24ddf7
Aleks <lai@tut.by>
2022-04-26 16:44:51 +0300

    added locale ru_RU

---
+++ lib/src/locale/ru_RU.dart
@@ -0,0 +1,42 @@
+part of vcl;
+
+final locale_ru_RU =
+{
+  SysLocale.SYSTEM:
+  {
+    SysLocale.DIALOGS:
+    {
+      '${ SysLocaleDialogs.ERROR }': 'Ошибка',
+      '${ SysLocaleDialogs.WARNING }': 'Внимание',
+
+      SysLocale.RESULTS:
+      {
+        '${ TModalResult.Ok }': 'OK',
+        '${ TModalResult.Cancel }': 'Отмена',
+        '${ TModalResult.Abort }': 'Прервать',
+        '${ TModalResult.Retry }': 'Повторить',
+        '${ TModalResult.Ignore }': 'Пропустить',
+        '${ TModalResult.Yes }': 'Да',
+        '${ TModalResult.No }': 'Нет',
+        '${ TModalResult.All }': 'Все',
+        '${ TModalResult.NoToAll }': 'Нет для всех',
+        '${ TModalResult.YesToAll }': 'Да для всех',
+      }
+    },
+
+    SysLocale.RICH_EDIT:
+    {
+      SysLocale.TOOLS:
+      {
+        '${ TRichEditTools.ID_BOLD }': 'Полужирный',
+        '${ TRichEditTools.ID_ITALIC }': 'Курсив',
+        '${ TRichEditTools.ID_UNDERLINE }': 'Подчеркнутый',
+
+        '${ TRichEditTools.ID_JUSTIFY_LEFT }': 'По левому краю',
+        '${ TRichEditTools.ID_JUSTIFY_CENTER }': 'По центру',
+        '${ TRichEditTools.ID_JUSTIFY_RIGHT }': 'По правому краю',
+        '${ TRichEditTools.ID_JUSTIFY_FULL }': 'По ширине',
+      }
+    }
+  },
+};
