391640e0db282cca030630ae39a696557021f800
Aleks <lai@tut.by>
2022-04-26 16:44:33 +0300

    added locale en_EN

---
+++ lib/src/locale/en_EN.dart
@@ -0,0 +1,42 @@
+part of vcl;
+
+final locale_en_EN =
+{
+  SysLocale.SYSTEM:
+  {
+    SysLocale.DIALOGS:
+    {
+      '${ SysLocaleDialogs.ERROR }': 'Error',
+      '${ SysLocaleDialogs.WARNING }': 'Warning',
+
+      SysLocale.RESULTS:
+      {
+        '${ TModalResult.Ok }': 'OK',
+        '${ TModalResult.Cancel }': 'Cancel',
+        '${ TModalResult.Abort }': 'Abort',
+        '${ TModalResult.Retry }': 'Retry',
+        '${ TModalResult.Ignore }': 'Ignore',
+        '${ TModalResult.Yes }': 'Yes',
+        '${ TModalResult.No }': 'No',
+        '${ TModalResult.All }': 'All',
+        '${ TModalResult.NoToAll }': 'No to all',
+        '${ TModalResult.YesToAll }': 'Yes to all',
+      }
+    },
+
+    SysLocale.RICH_EDIT:
+    {
+      SysLocale.TOOLS:
+      {
+        '${ TRichEditTools.ID_BOLD }': 'Bold',
+        '${ TRichEditTools.ID_ITALIC }': 'Italic',
+        '${ TRichEditTools.ID_UNDERLINE }': 'Underline',
+
+        '${ TRichEditTools.ID_JUSTIFY_LEFT }': 'Justify left',
+        '${ TRichEditTools.ID_JUSTIFY_CENTER }': 'Justify right',
+        '${ TRichEditTools.ID_JUSTIFY_RIGHT }': 'Justify center',
+        '${ TRichEditTools.ID_JUSTIFY_FULL }': 'Justify',
+      }
+    }
+  },
+};
